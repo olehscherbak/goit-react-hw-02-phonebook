b0b1412056ad6712177014998e0d39b8728347f3
Oleh.Scherbak <oleh.scherbak@ukr.net>
2023-02-02 00:27:56 +0200

    add initial components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,67 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: '',
+  };
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+    const newContact = {
+      id: nanoid(),
+      name: evt.target.name.value,
+      number: '',
+    };
+
+    this.setState(({ name, contacts }) => ({
+      contacts: [newContact, ...contacts],
+      name: '',
+    }));
+  };
+
+  handleChange = evt => {
+    this.setState({ name: evt.target.value });
+  };
+
+  render() {
+    const { contacts } = this.state;
+    return (
+      <>
+        <h2>Phonebook</h2>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={this.handleChange}
+            value={this.state.name}
+          />
+          <button type="submit">Add contact</button>
+        </form>
+        <h2>Contacts</h2>
+        <ul>
+          {contacts.map(contact => {
+            return (
+              <li key={contact.id}>
+                {contact.name} {contact.number}
+              </li>
+            );
+          })}
+        </ul>
+      </>
+    );
+  }
+}
+
+export default App;
